07ac44828e939834c3929f35ee7eda29ab29a041
Jerome Rojas <skreamlight@gmail.com>
2023-12-09 02:04:42 -0400

    update 0.0.4

--- main.py
+++ main.py
@@ -1,9 +1,95 @@
-from structure import *
-from structure import GameClass
+import glm
+import sdl2
+import sdl2.ext 
+import sdl2.sdlimage
+import time
+class GameClass:
+    def __init__(self):
+        sdl2.ext.init()
+        
+        #set display for fullScreen
+        display = sdl2.SDL_DisplayMode()
+        sdl2.SDL_GetCurrentDisplayMode(0, display)
+        self.window = sdl2.ext.Window("Hello world", size=(display.w, display.h))
+        self.window.show()
+        
+        #create global render so loop doesn't breaks
+        self.renderer = sdl2.SDL_CreateRenderer(self.window.window, -1, sdl2.SDL_RENDERER_PRESENTVSYNC | sdl2.SDL_RENDERER_ACCELERATED)
+        self.running = True
+        
+        #set instance of window to full screen
+        sdl2.SDL_SetWindowFullscreen(self.window.window, sdl2.SDL_WINDOW_FULLSCREEN)
+        self.fps = 60
+        self.ms_per_frame = 1000 / self.fps
+        self.mspreviousframe = 0
+                
+    def run(self):
+        self.setup()    
+        while self.running:
+            self.events = sdl2.ext.get_events()
+            for event in self.events:
+                if event.type == sdl2.SDL_QUIT:
+                    self.running = False
+                    break
+            self.p_input()   
+            self.update()
+            self.render()
+        self.destroy()
 
-instance =  GameClass()
-instance.run()
-instance.p_input()
-instance.update()
-instance.render()
+    #proccess inputs 
+    def p_input(self):
+        for event in self.events:
+            if event.type == sdl2.SDL_QUIT:
+                self.running = False
+                break
 
+            elif event.type == sdl2.SDL_KEYDOWN:
+                key = event.key.keysym.sym == sdl2.SDLK_ESCAPE
+                self.running = False
+                break
+    
+    #initialize game objects
+    def setup(self):
+        self.playerpos = glm.vec2(10, 20)
+        self.playervel = glm.vec2(50, 5)
+    
+    #updates window
+    def update(self):
+        # condition to manage target time per frame 
+        # sdl.delay will use way less CPU resources
+        self.ms_prev_frame = sdl2.SDL_GetTicks()
+        WaitTime = self.ms_per_frame - (sdl2.SDL_GetTicks() - self.ms_prev_frame)
+        
+        if WaitTime > 0 or WaitTime <= self.ms_per_frame:
+            sdl2.SDL_Delay(int(WaitTime))
+        
+        # deltatime = diff in ticks since last frame conv to seconds
+        self.dt = (sdl2.SDL_GetTicks() - self.ms_prev_frame)/1000
+        print(sdl2.SDL_GetTicks(), self.ms_prev_frame, self.dt)
+
+        self.playerpos.x += self.playervel.x * self.dt
+        self.playerpos.y += self.playervel.y * self.dt
+
+    def render(self):
+        sdl2.SDL_SetRenderDrawColor(self.renderer, 21, 21, 21, 255)
+        sdl2.SDL_RenderClear(self.renderer)
+        
+        #load path parameter as bytes so IMG_Load works
+        surface = sdl2.sdlimage.IMG_Load(b"./assets/images/tank-tiger-right.png")
+        texture = sdl2.SDL_CreateTextureFromSurface(self.renderer, surface)
+        sdl2.SDL_FreeSurface(surface)
+
+        #rectangle destination of texture
+        dstRect = sdl2.SDL_Rect(int(self.playerpos.x), int(self.playerpos.y), 200, 200)
+        sdl2.SDL_RenderCopy(self.renderer, texture, None, dstRect)
+        sdl2.SDL_DestroyTexture(texture)
+        
+        sdl2.SDL_RenderPresent(self.renderer)
+        
+    #destroys window
+    def destroy(self):
+        print("game destructor was called!")
+        sdl2.ext.quit()
+    
+game = GameClass()
+game.run()
